011b36909cb6a4fcb1466d62bdbc81e0de37f1b9
Sabokhat Kalandarova <sabohat2802@gmail.com>
2020-03-09 11:29:13 +0500

    feat: code ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
+if(array != undefined && array.length != 0){
+  return Math.min(...array); 
+} else {
   return 0;
-}
+}}
 
 exports.max = function max (array) {
-  return 0;
+  if(array != undefined && array.length != 0) {return Math.max(...array);}
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
+  let sum = 0;
+  let countM =0;
+  if(array != undefined && array.length != 0) {
+for(i=0; i<array.length; i++ ){
+sum += array[i];
+countM++;
+}
+return sum/countM;
+
+}else {
   return 0;
+}
 }
